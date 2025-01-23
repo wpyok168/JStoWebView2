c3ff77447fab24ab617f272b5e3a83c4e4766d1a
wpyok500 <wpyok168@gmail.com>
2025-01-23 23:48:55 +0800

    添加项目文件。

---
+++ JStoWebView2/CustomWebView2HostObject.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JStoWebView2
+{
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ComVisible(true)]
+    public class CustomWebView2HostObject
+    {
+        public string TestCalcAddByCsharpMethod(int num1, int num2, string message)
+        {
+            MessageBox.Show($"C#方法接收到J传入的参数 num1={num1}，num2={num2}，message={message}", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            return "计算结果为:" + (num1 + num2);
+        }
+    }
+
+
+    // Bridge and BridgeAnotherClass are C# classes that implement IDispatch and works with AddHostObjectToScript.
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ComVisible(true)]
+    public class BridgeAnotherClass
+    {
+        // Sample property.
+        public string Prop { get; set; } = "Example";
+    }
+
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ComVisible(true)]
+    public class Bridge
+    {
+        public string Func(string param)
+        {
+            return "Example: " + param;
+        }
+
+        public BridgeAnotherClass AnotherObject { get; set; } = new BridgeAnotherClass();
+
+        // Sample indexed property.
+        [System.Runtime.CompilerServices.IndexerName("Items")]
+        public string this[int index]
+        {
+            get { return m_dictionary[index]; }
+            set { m_dictionary[index] = value; }
+        }
+        private Dictionary<int, string> m_dictionary = new Dictionary<int, string>();
+    }
+}
---
+++ JStoWebView2/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.WinForms;
+
+namespace JStoWebView2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        WebView2 wv=new WebView2();
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            //https://learn.microsoft.com/zh-cn/dotnet/api/microsoft.web.webview2.core.corewebview2.addhostobjecttoscript?view=webview2-dotnet-1.0.1418.22
+
+            this.Controls.Add(wv);
+            this.wv.Dock = DockStyle.Fill;
+            await this.wv.EnsureCoreWebView2Async();
+
+            //注册winning脚本c#互操作  注意注册的名称winning要和前端window.chrome.webview.hostObjects.winning;保持一致
+            this.wv.CoreWebView2.AddHostObjectToScript("winning", new CustomWebView2HostObject());
+            //注册全局变量winning
+            //await this.wv.CoreWebView2.AddScriptToExecuteOnDocumentCreatedAsync("var hostobject =window.chrome.webview.hostObjects.winning;");
+            this.wv.CoreWebView2.AddHostObjectToScript("bridge", new Bridge());
+
+
+            this.wv.CoreWebView2.Navigate("https://visualsupport.microsoft.com/s5lTRSI/activate/result");
+            //this.wv.Source = new Uri("https://www.baidu.com");
+
+            /* 前端控制台调用——注意使用异步调用
+               async function jscallcshart() {
+                  var winning =window.chrome.webview.hostObjects.winning;
+                  var result = await winning.TestCalcAddByCsharpMethod(10, 13, "加法计算");
+                  console.log(result);
+                }
+                jscallcshart();
+             *
+             *
+             */
+
+        }
+    }
+}
